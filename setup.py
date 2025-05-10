dbbf9bc7349e790575c2e24a7b851418ff4660b2
goldenglorys <olusolagloryolamide@gmail.com>
2025-05-10 21:24:49 +0000

    update

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
-# from distutils.core import setup
-
-# if __name__ == "__main__":
-#     setup()
-
-
 from setuptools import setup, find_packages
 
-package_name = 'rosgpt'
-
 setup(
-    name=package_name,
+    name='rosgpt',
     version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
@@ -21,14 +13,4 @@ setup(
         'rich',
         'regex',
     ],
-    author='ROSGPT Team',
-    author_email='example@example.com',
-    description='ROS Guide Powered by Transformers',
-    license='MIT',
-    entry_points={
-        'console_scripts': [
-            'turtle_agent = turtle_agent.scripts.turtle_agent:main',
-            'pincherx_agent = pincherx_agent.scripts.pincherx_agent:main',
-        ],
-    },
 )
